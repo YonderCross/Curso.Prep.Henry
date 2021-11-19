a2f929a08eab62ddd7e80021b956751e824f8bc8
YonderCross <verasm301296@hotmail.com>
2021-11-18 18:53:30 -0600

    Tarea 2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -227,12 +227,12 @@ function esVocal(letra){
   //Escribe tu código aquí
 
   if (letra.length > 1){
-    return "Dato incorrecto"
+    return "Dato incorrecto";
   } 
   else if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u" ){
     return "Es vocal"
   }
-   return "Dato incorrecto"
+   return "Dato incorrecto";
 }
 
 
